d6dd35f8d832cdfb88c45034c5ebb654be02ed1a
Matt Davis <mdavis@redventures.com>
2024-10-09 13:26:35 -0400

    Added Expand property to section block, Option to set value, and tests

--- block_section_test.go
+++ block_section_test.go
@@ -10,13 +10,14 @@ func TestNewSectionBlock(t *testing.T) {
 
 	textInfo := NewTextBlockObject("mrkdwn", "*<fakeLink.toHotelPage.com|The Ritz-Carlton New Orleans>*\n★★★★★\n$340 per night\nRated: 9.1 - Excellent", false, false)
 
-	sectionBlock := NewSectionBlock(textInfo, nil, nil, SectionBlockOptionBlockID("test_block"))
+	sectionBlock := NewSectionBlock(textInfo, nil, nil, SectionBlockOptionBlockID("test_block"), SectionBlockOptionExpand(true))
 	assert.Equal(t, string(sectionBlock.Type), "section")
 	assert.Equal(t, sectionBlock.BlockID, "test_block")
 	assert.Equal(t, len(sectionBlock.Fields), 0)
 	assert.Nil(t, sectionBlock.Accessory)
 	assert.Equal(t, sectionBlock.Text.Type, "mrkdwn")
 	assert.Contains(t, sectionBlock.Text.Text, "New Orleans")
+	assert.Equal(t, sectionBlock.Expand, true)
 
 }
 
